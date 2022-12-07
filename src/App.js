48d39f953b4cf9bc1ae7768b780781564d9d882d
muhammadshehzaib <shehzaibbutt222@gmail.com>
2022-12-07 21:27:47 +0500

    React query

--- src/App.js
+++ src/App.js
@@ -4,19 +4,26 @@ import Home from "./pages/Home"
 import Layout from "./pages/Layout"
 import Contact from "./pages/Contact"
 import Navbar from './pages/Navbar'
+import {QueryClient,QueryClientProvider} from "react-query"
 
 const App = () => {
+const client = new QueryClient({defaultOptions:{
+   queries:{
+    refetchOnWindowFocus:false
+   }
+}})
   return (
     <div>
+      <QueryClientProvider client={client}>
       <Router>
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path='/layout' element={<Layout />} />
           <Route path='/contact' element={<Contact />} />
-          
         </Routes>
       </Router>
+      </QueryClientProvider>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import {useQuery } from 'react-query'
+import Axios from "axios"
 
 const Home = () => {
+  const { data, isLoading, error,refetch } = useQuery(['cat'], () => {
+    return Axios.get("https://catfact.ninja/fact").then(res => res.data)
+  });
+  if (error) {
+    return <h1>Sorry, There was an error</h1>
+  }
+  if (isLoading) {
+    return <h1>Loading...</h1>
+  }
   return (
-    <div>This is Home </div>
+    <div>This is Home
+      <p>{data?.fact}</p>
+      <button onClick={refetch}>Update Data </button>
+    </div>
   )
 }
 
